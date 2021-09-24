5262e9f1ca7b776f3c85af681d067c59debae38a
Kent Zhang <kentzhang31@yahoo.com>
2021-09-24 12:07:06 -0700

    finish setting up

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route } from "react-router";
-import { AuthRoute } from "../util/route_util";
+// import { AuthRoute } from "../util/route_util";
 
 const App = () => (
   <div>
