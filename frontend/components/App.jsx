61d5d525be7030d5ff4db4495ee1346a11e6b8f1
Kent Zhang <kentzhang31@yahoo.com>
2021-09-28 14:12:15 -0700

    fixed minor issues with user-auth functionality

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -5,6 +5,7 @@ import SignupFormContainer from "../components/session_form/signup_form_containe
 import { AuthRoute } from "../util/route_util";
 import NavBarContainer from "./nav_bar/nav_bar_container";
 import Home from "./home/home";
+import ErrorPage from "./error/error";
 
 const App = () => (
   <div>
@@ -12,9 +13,11 @@ const App = () => (
     </header>
 
     <Switch>
+      <Route exact path="/" component={Home} />
       <AuthRoute path="/login" component={LoginFormContainer} />
       <AuthRoute path="/signup" component={SignupFormContainer} />
-      <Route path="/" component={Home} />
+      <Route component={ErrorPage}/>
+
     </Switch>
 
   </div>
