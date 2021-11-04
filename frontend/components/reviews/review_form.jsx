e6d4db91709418afd046eba1978c0c9513d60558
Kent Zhang <kentzhang31@yahoo.com>
2021-11-04 12:58:52 -0700

    styling review form

--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -63,43 +63,60 @@ class ReviewForm extends React.Component {
       )
     });
 
-    return(
-      <div>
+    return (
+      <div className="form-all-container">
         <NavSearchBarContainer />
-        <div className="all-ratings">
-          <div className={`review-5star`} id='select-review' onClick={() => this.toggleRating(5)}>
+        <div className="rating-with-form">
+          <div className="all-ratings">
+            <div
+              className={`review-5star`}
+              id="select-review"
+              onClick={() => this.toggleRating(5)}
+            ></div>
+            <div
+              className={`review-4star`}
+              id="select-review"
+              onClick={() => this.toggleRating(4)}
+            ></div>
+            <div
+              className={`review-3star`}
+              id="select-review"
+              onClick={() => this.toggleRating(3)}
+            ></div>
+            <div
+              className={`review-2star`}
+              id="select-review"
+              onClick={() => this.toggleRating(2)}
+            ></div>
+            <div
+              className={`review-1star`}
+              id="select-review"
+              onClick={() => this.toggleRating(1)}
+            ></div>
           </div>
-          <div className={`review-4star`} id='select-review' onClick={() => this.toggleRating(4)}>
+          <div className="review-forms-container">
+            <h1>{business.name}</h1>
+            <div
+              className={`review-${this.state.rating}star`}
+              id="chosen-rating"
+            ></div>
+            <form onSubmit={this.handleSubmit}>
+              <textarea
+                className="review-form-body"
+                placeholder="Great place to eat!"
+                value={this.state.body}
+                onChange={this.update("body")}
+              />
+              <button>{formType}</button>
+            </form>
           </div>
-          <div className={`review-3star`} id='select-review' onClick={() => this.toggleRating(3)}>
-          </div>
-          <div className={`review-2star`} id='select-review' onClick={() => this.toggleRating(2)}>
-          </div>
-          <div className={`review-1star`} id='select-review' onClick={() => this.toggleRating(1)}>
-          </div>
-        </div>
-        <div className="review-forms-container">
-          <h1>
-            {business.name}
-          </h1>
-          <div className={`review-${this.state.rating}star`} id="chosen-rating">
-          </div>
-          <form onSubmit={this.handleSubmit}>
-            <textarea
-              className="review-form-body"
-              placeholder="Great place to eat!"
-              value={this.state.body}
-              onChange={this.update('body')}
-            />
-            <button>{formType}</button>
-          </form>
         </div>
         <div className="recent-reviews">
           <h2>Recent Reviews</h2>
           {displayRecentReviews}
         </div>
       </div>
-    )
+    );
   }
 }
 
